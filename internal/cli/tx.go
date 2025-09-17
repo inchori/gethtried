013b05a44fc1fbee9dfee9db79b6cadd3d419f77
inchori <sic61695@gmail.com>
2025-09-17 23:06:38 +0900

    feat: add tx trie tmp

--- internal/cli/tx.go
+++ internal/cli/tx.go
@@ -1,1 +1,43 @@
 package cli
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	gethtrie "github.com/ethereum/go-ethereum/trie"
+	"github.com/inchori/geth-state-trie/internal/geth"
+	"github.com/spf13/cobra"
+)
+
+var txCmd = &cobra.Command{
+	Use:   "tx",
+	Short: "Visualize the transaction trie for a specific block height",
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := geth.NewEthClient(rpcURL)
+		if err != nil {
+			log.Fatalf("Failed to initialize eth client: %v", err)
+		}
+
+		block, err := client.GetBlockByNumber(context.Background(), blockHeight)
+		if err != nil {
+			log.Fatalf("Failed to get block by height: %v", err)
+		}
+		expectedRoot := block.Header().TxHash
+		transactions := block.Transactions()
+		calculatedRoot := gethtypes.DeriveSha(transactions, new(gethtrie.Trie))
+
+		fmt.Printf("Block Header TxRoot: %s\n", expectedRoot.Hex())
+		fmt.Printf("Calculated TxRoot:   %s\n", calculatedRoot.Hex())
+		if expectedRoot == calculatedRoot {
+			fmt.Println("✅ Verification Successful!")
+		} else {
+			fmt.Println("❌ Verification FAILED!")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(txCmd)
+}
